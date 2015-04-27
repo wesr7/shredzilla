9bf67b0178d7380b243e849bc043bfb1f8df156c
wesr7 <jwesleyrodgers@gmail.com>
2015-04-27 09:46:55 -0700

    colored background. more sytling

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -3,23 +3,26 @@ class ExercisesController < ApplicationController
     def create
         @workout = Workout.find(params[:workout_id])
         @exercise = @workout.exercises.create(exercises_params)
-        if @exercise
-            redirect_to(workout_path(@workout))
+           @workout.user = current_user
+        if @exercise.save
+            redirect_to edit_workout_path(@workout)
         else
-            render(workout_path(@workout))
+            render workout_path(@workout)
     end
 end
 
 def edit
     @workout = Workout.find(params[:workout_id])
-    @exercise = @workout.exercise.find(params[:id])
+    @exercise = @workout.exercises.find(params[:id])
+    @workout.user = current_user
 end
 
 def update
     @workout = Workout.find(params[:workout_id])
-    @exercise = @workout.exercise.fine(params[:id])
-    if @exercise.update_attributes(exercises_params)
-        redirect_to(workout_path(@workout))
+    @exercise = @workout.exercises.find(params[:id])
+    @workout.user = current_user
+    if @exercise.update(exercises_params)
+        redirect_to edit_workout_path(@workout)
     else
         flash.now[:error] = "Could not update exercise"
  end
@@ -30,7 +33,7 @@ def destroy
     @workout = Workout.find(params[:workout_id])
     @exercise = @workout.exercises.find(params[:id])
     if @exercise.destroy
-        redirect_to(workout_path(@workout))
+        redirect_to edit_workout_path(@workout)
     else
         flash.now[:error] = "Could not delete exercise!"
     end
--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,59 +1,62 @@
 class WorkoutsController < ApplicationController
 
   def index
-    @workout = Workout.all
+    @workouts = Workout.all
   end
 
- def show
-    @workout = Workout.find(params[:id])
-    @exercise = Exercise.new
-    @exercises = @workout.exercises
-  end
 
   def new
     @workout = Workout.new
   end
 
+  def edit
+  @workout = Workout.find(params[:id])
+  @exercise = Exercise.new
+  @workout.user = current_user
+  @exercises = @workout.exercises
+end
+
   def create
-    @workout = Workout.new(workout_params)
+    @workout = Workout.new(workouts_params)
     @workout.user = current_user
     if @workout.save
-      redirect_to workout_path(@workout)
+      redirect_to edit_workout_path(@workout)
     else
       render "new"
   end
 end
 
-def edit
-  @workout = Workout.find(params[:id])
-  @exercise = Exercise.new
-  @exercises = @workout.exercises
-end
+def show
+    @workout = Workout.find(params[:id])
+    @exercise = Exercise.new
+    @exercises = @workout.exercises
+  end
 
 
   def update
     @workout = Workout.find(params[:id])
-    if @workout.update_attributes(workout_params)
-    redirect_to workout_path
+    @workout.user = current_user
+    if @workout.update(workouts_params)
+    redirect_to edit_workout_path(@workout)
   else
-    render '/'
+    render workouts_path
   end
   end
 
   def destroy
     @workout = Workout.find(params[:id])
-    @exercises = Exercise.find(params[:id])
+
     if @workout.destroy
     redirect_to workouts_path
   else
-      redirect_to workout_path
+      redirect_to workout_path(@workout)
     end
   end
 
 
 
      private
-     def workout_params
+     def workouts_params
       params.require(:workout).permit(:note, :name)
     end
 
